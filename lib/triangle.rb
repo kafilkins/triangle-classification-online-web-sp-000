71a758f344577fd1cb68733779d1fba8786bc8ad
Keith Filkins <kafilkins@me.com>
2020-12-23 18:23:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,8 +27,8 @@ class Triangle
         false
       else
         true
-      
-        
+
+
 
       real_triangle << false if side <= 0
       raise TriangleError if real_triangle.include?(false)
