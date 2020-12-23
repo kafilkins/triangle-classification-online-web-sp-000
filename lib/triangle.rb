1abc6d6094e10b722ac099612fc6c1c55635c54b
Keith Filkins <kafilkins@me.com>
2020-12-23 18:17:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
 
   def kind
     validate_triangle
-    if a == b && b == c 
+    if a == b && b == c
       :equilateral
-    elsif a == b || b == c || a == c 
+    elsif a == b || b == c || a == c
       :isosceles
     else
       :scalene
